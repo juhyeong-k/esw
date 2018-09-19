6adce05f4abfd7dafdf893c8be19554d2a1b1e97
Juhyeong Kim <Juhyeong_k@naver.com>
2018-09-19 15:57:49 +0900

    back up

--- main.cpp
+++ main.cpp
@@ -45,8 +45,8 @@ bool selectObject = false;
 int trackObject = 0;
 bool showHist = true;
 Point origin;
-Rect selection(150,90,60,60);
-int vmin = 10, vmax = 256, smin = 30;
+Rect selection(140,100,40,40);
+int vmin = 50, vmax = 256, smin = 50;
 
 /**
   * @brief  Required threads, functions, and structures.\
@@ -185,7 +185,7 @@ void * main_thread(void *arg)
     int hsize = 16;
     float hranges[] = {0,180};
     const float* phranges = hranges;
-
+    trackObject = -1;
     while(1)
     {   
         gettimeofday(&st, NULL);
@@ -206,44 +206,50 @@ void * main_thread(void *arg)
          */
         Mat image(VPE_OUTPUT_H, VPE_OUTPUT_W, CV_8UC3, omap_bo_map(capt->bo[0]));
         cvtColor(image, hsv, COLOR_BGR2HSV);
-        int _vmin = vmin, _vmax = vmax;
-        inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)), Scalar(180, 256, MAX(_vmin, _vmax)), mask);
-        int ch[] = {0, 0};
-        hue.create(hsv.size(), hsv.depth());
-        mixChannels(&hsv, 1, &hue, 1, ch, 1);
-        Mat roi(hue, selection), maskroi(mask, selection);
-        calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
-        normalize(hist, hist, 0, 255, NORM_MINMAX);
-        trackWindow = selection;
-        trackObject = 1; // Don't set up again, unless user selects new ROI
-        histimg = Scalar::all(0);
-        int binW = histimg.cols / hsize;
-        Mat buf(1, hsize, CV_8UC3);
-        for( int i = 0; i < hsize; i++ )
-            buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
-        cvtColor(buf, buf, COLOR_HSV2BGR);
-        for( int i = 0; i < hsize; i++ )
+        if(trackObject)
         {
-            int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
-            rectangle( histimg, Point(i*binW,histimg.rows),
-                       Point((i+1)*binW,histimg.rows - val),
-                       Scalar(buf.at<Vec3b>(i)), -1, 8 );
+			int _vmin = vmin, _vmax = vmax;
+	        inRange(hsv, Scalar(0, smin, MIN(_vmin,_vmax)), Scalar(180, 256, MAX(_vmin, _vmax)), mask);
+	        int ch[] = {0, 0};
+	        hue.create(hsv.size(), hsv.depth());
+	        mixChannels(&hsv, 1, &hue, 1, ch, 1);
+	        if(trackObject < 0)
+	        {
+	        	Mat roi(hue, selection), maskroi(mask, selection);
+		        calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
+		        normalize(hist, hist, 0, 255, NORM_MINMAX);
+		        trackWindow = selection;
+		        trackObject = 1; // Don't set up again, unless user selects new ROI
+		        histimg = Scalar::all(0);
+		        int binW = histimg.cols / hsize;
+		        Mat buf(1, hsize, CV_8UC3);
+		        for( int i = 0; i < hsize; i++ )
+		            buf.at<Vec3b>(i) = Vec3b(saturate_cast<uchar>(i*180./hsize), 255, 255);
+		        cvtColor(buf, buf, COLOR_HSV2BGR);
+		        for( int i = 0; i < hsize; i++ )
+		        {
+		            int val = saturate_cast<int>(hist.at<float>(i)*histimg.rows/255);
+		            rectangle( histimg, Point(i*binW,histimg.rows),
+	                Point((i+1)*binW,histimg.rows - val),
+	                Scalar(buf.at<Vec3b>(i)), -1, 8 );
+		        }
+	        }
+	        // Perform CAMShift
+	        calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
+	        backproj &= mask;
+	        IplImage tmp=backproj;
+	        int trackBox = cvCamShift(&tmp, trackWindow, TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ), &comp, NULL);
+	        if( trackWindow.area() <= 1 )
+	        {
+	            int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
+	            trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
+	                               trackWindow.x + r, trackWindow.y + r) &
+	                          Rect(0, 0, cols, rows);
+	        }
+	        if( backprojMode )
+	            cvtColor( backproj, image, COLOR_GRAY2BGR );
+	        rectangle(image, comp.rect, Scalar(0,0,255));
         }
-        // Perform CAMShift
-        calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
-        backproj &= mask;
-        IplImage tmp=backproj;
-        int trackBox = cvCamShift(&tmp, trackWindow, TermCriteria( TermCriteria::EPS | TermCriteria::COUNT, 10, 1 ), &comp, NULL);
-        if( trackWindow.area() <= 1 )
-        {
-            int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5)/6;
-            trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
-                               trackWindow.x + r, trackWindow.y + r) &
-                          Rect(0, 0, cols, rows);
-        }
-        if( backprojMode )
-            cvtColor( backproj, image, COLOR_GRAY2BGR );
-        rectangle(image, comp.rect, Scalar(0,0,255));
         if( selectObject && selection.width > 0 && selection.height > 0 )
         {
             Mat roi(image, selection);
