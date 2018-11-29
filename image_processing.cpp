e5f8df92c3372e2b4a6e104cb439a3bd2f180663
Juhyeong Kim <Juhyeong_k@naver.com>
2018-11-30 06:53:04 +0900

    Update HSV Convert

--- image_processing.cpp
+++ image_processing.cpp
@@ -12,7 +12,7 @@ extern std::ofstream fileout;
 uint8_t BGR24_to_HSV::getMaxBGR_VBGR(uint8_t b, uint8_t g, uint8_t r, uint8_t *V_BGR) {
     // V_BGR : B = 0, G = 1, R = 2.
     *V_BGR = 0;
-    uint8_t max = isBlue;
+    uint8_t max = b;
     if (g > max) { max = g; *V_BGR = isGreen; }
     if (r > max) { max = r; *V_BGR = isRed; }
     return max;
